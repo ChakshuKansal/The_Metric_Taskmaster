18699ea595b998b071afbcbe04a8606a3ebaa4db
chakshukansal <ckansal8174@gmail.com>
2024-09-20 00:45:58 +0530

    Added User Tasks inputs

--- Main/script.js
+++ Main/script.js
@@ -15,44 +15,24 @@ function logout() {
   window.location.href = "../Login/login.html";
 }
 
-function openDashboard() {
-  // Redirect to the dashboard page
-  window.location.href = "../Dashboard/dashboard.html";
-}
-
 function openCalender() {
-  // window.location.href="../Calender/Calender.html";
   window.location.href = "../Calender/Calender.html";
 }
 
-function displayRegisteredUsers() {
-  const usersList = JSON.parse(localStorage.getItem("usersList")) || [];
+let createtask = document.getElementById("createtask");
+let taskinput = document.getElementById("taskinput");
+let xmark = document.getElementById("Xmark");
 
-  const usersListElement = document.getElementById("usersList");
-  usersListElement.innerHTML = ""; // Clear the list first
-
-  usersList.forEach((username) => {
-    const userDiv = document.createElement("div");
-    userDiv.classList.add("user-item");
-    userDiv.textContent = username;
-    usersListElement.appendChild(userDiv);
-  });
+function submitdata() {
+  const headinput = document.getElementById("headinput").value;
+  const desc = document.getElementById("taskinputbox").value;
+  localStorage.setItem(headinput, desc);
 }
 
-// Function to load a page into the main content area
-function loadPage(page) {
-  fetch(page)
-    .then((response) => response.text())
-    .then((data) => {
-      document.getElementById("main-content").innerHTML = data;
+createtask.addEventListener("click", () => {
+  taskinput.style.right = 0;
+});
 
-      if (page.includes("dashboard.html")) {
-        displayRegisteredUsers();
-      }
-    })
-    .catch((error) => {
-      console.error("Error loading page:", error);
-      document.getElementById("main-content").innerHTML =
-        "<p>Error loading page.</p>";
-    });
-}
+xmark.addEventListener("click", () => {
+  taskinput.style.right = "-20%";
+});
