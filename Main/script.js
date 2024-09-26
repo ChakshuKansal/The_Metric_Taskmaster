17524d3b197f426f73745fe22b012c4cb216c68e
chakshukansal <ckansal8174@gmail.com>
2024-09-26 23:17:45 +0530

    Added Pomodoro Timer Option

--- Main/script.js
+++ Main/script.js
@@ -2,7 +2,9 @@
 let createtask = document.getElementById("createtask");
 let taskinput = document.getElementById("taskinput");
 let xmark = document.getElementById("Xmark");
-
+let timerstart=document.getElementById("timerstart");
+let timercont=document.getElementById("timercont");
+let overlay=document.getElementById("overlay")
 
 function displayUsername() {
   const currentUser = localStorage.getItem("currentUser");
@@ -11,11 +13,23 @@ function displayUsername() {
   }
 }
 
+
 window.addEventListener('load',()=>{
   displayUsername();
   updateAnalytics();
   loadLastSearchedCity(); // Show weather for last searched city on page load
   displayTasks();
+})
+
+timerstart.addEventListener('click',()=>{
+  timercont.style.display="block";
+  overlay.style.display="block";
+})
+
+overlay.addEventListener("click",()=>{
+  timercont.style.display="none";
+  overlay.style.display="none";
+
 })
 
 function logout() {
@@ -146,7 +160,7 @@ function getTasks() {
 
 // Function to format the description into multiple lines
 function formatDescription(description) {
-  const lineLength = 18; // Number of characters per line
+  const lineLength = 78; // Number of characters per line
   let formattedDescription = "";
 
   for (let i = 0; i < description.length; i += lineLength) {
@@ -385,3 +399,115 @@ function dateBuilder(d) {
 
   return `${day} ${date} ${month} ${year}`;
 }
+
+
+// ///////////////////////Timer
+
+let focusButton = document.getElementById("focus");
+let buttons = document.querySelectorAll(".btn");
+let shortBreakButton = document.getElementById("shortbreak");
+let longBreakButton = document.getElementById("longbreak");
+let startBtn = document.getElementById("btn-start");
+let reset = document.getElementById("btn-reset");
+let pause = document.getElementById("btn-pause");
+let time = document.getElementById("time");
+let set;
+let active = "focus";
+let count = 59;
+let paused = true;
+let minCount = 24;
+time.textContent = `${minCount + 1}:00`;
+
+const appendZero = (value) => {
+  value = value < 10 ? `0${value}` : value;
+  return value;
+};
+
+reset.addEventListener(
+  "click",
+  (resetTime = () => {
+    pauseTimer();
+    switch (active) {
+      case "long":
+        minCount = 14;
+        break;
+      case "short":
+        minCount = 4;
+        break;
+      default:
+        minCount = 24;
+        break;
+    }
+    count = 59;
+    time.textContent = `${minCount + 1}:00`;
+  })
+);
+
+const removeFocus = () => {
+  buttons.forEach((btn) => {
+    btn.classList.remove("btn-focus");
+  });
+};
+
+focusButton.addEventListener("click", () => {
+  removeFocus();
+  focusButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 24;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+
+shortBreakButton.addEventListener("click", () => {
+  active = "short";
+  removeFocus();
+  shortBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 4;
+  count = 59;
+  time.textContent = `${appendZero(minCount + 1)}:00`;
+});
+
+longBreakButton.addEventListener("click", () => {
+  active = "long";
+  removeFocus();
+  longBreakButton.classList.add("btn-focus");
+  pauseTimer();
+  minCount = 14;
+  count = 59;
+  time.textContent = `${minCount + 1}:00`;
+});
+
+pause.addEventListener(
+  "click",
+  (pauseTimer = () => {
+    paused = true;
+    clearInterval(set);
+    startBtn.classList.remove("hide");
+    pause.classList.remove("show");
+    reset.classList.remove("show");
+  })
+);
+
+startBtn.addEventListener("click", () => {
+  reset.classList.add("show");
+  pause.classList.add("show");
+  startBtn.classList.add("hide");
+  startBtn.classList.remove("show");
+  if (paused) {
+    paused = false;
+    time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+    set = setInterval(() => {
+      count--;
+      time.textContent = `${appendZero(minCount)}:${appendZero(count)}`;
+      if (count == 0) {
+        if (minCount != 0) {
+          minCount--;
+          count = 60;
+        } else {
+          clearInterval(set);
+        }
+      }
+    }, 1000);
+  }
+});
